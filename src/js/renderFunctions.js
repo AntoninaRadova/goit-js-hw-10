890c517c31ccf16f67bf9f7f6e3e6e76e947825f
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-03-13 00:15:21 +0200

    Update renderFunctions.js

--- src/js/renderFunctions.js
+++ src/js/renderFunctions.js
@@ -1,8 +1,23 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+function makeCountriesListMarkup({ name, flags }) {
+    
+return `<li class="country-item">
+                <img class="country-flag-icon" src="${flags.svg}" />
+            <p class="country-name">${name.official}</p>
+        </li>`
 }
+
+function makeCountryCardMarkup({name, capital, population, flags, languages}) {
+
+    return `<div class="country-card">
+                <div class="card-head">
+                    <img class="flag" src="${flags.svg}" />
+                    <p class="card-country-name">${name.official}</p>
+                </div>
+                <p class="country-info"><span class="param">Capital:</span> ${capital}</p>
+                <p class="country-info"><span class="param">Population:</span> ${population}</p>
+                <p class="country-info"><span class="param">Languages:</span> ${Object.values(languages).join(", ")}</p>
+            </div>`
+}
+
+
+export default { makeCountriesListMarkup, makeCountryCardMarkup };
